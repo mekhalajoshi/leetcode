72a8b25e7dc2d48d4bd181887584b563250c02ed
Mekhala Joshi <mekhalajoshi@users.noreply.github.com>
2026-04-28 15:44:51 -0400

    Add: lru-cache - submission-2

---
+++ Data Structures & Algorithms/lru-cache/submission-2.js
@@ -0,0 +1,39 @@
+class LRUCache {
+    /**
+     * @param {number} capacity
+     */
+    constructor(capacity) {
+        this.capacity = capacity
+        this.map = new Map()
+    }
+
+    /**
+     * @param {number} key
+     * @return {number}
+     */
+    get(key) {
+       if(!this.map.has(key)) return -1;
+       let value = this.map.get(key);
+       this.map.delete(key);
+       this.map.set(key, value);
+       return value;
+    }
+
+    /**
+     * @param {number} key
+     * @param {number} value
+     * @return {void}
+     */
+    put(key, value) {
+        if(this.map.has(key)){
+            this.map.delete(key);
+        }
+        if(this.map.size === this.capacity){
+            const firstKey = this.map.keys().next().value;
+            this.map.delete(firstKey);
+        }
+        
+        this.map.set(key, value);
+        console.log(this.map)
+    }
+}
